c02346a321e45ebc0a64d9149e2dd6a1e4902534
Subhan383 <abdulsubhan0383@gmail.com>
2020-05-29 11:21:43 +0530

    Eureka Server to register Microservices For StoreProject

---
+++ eurekaserver/src/test/java/com/org/store/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.store.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
